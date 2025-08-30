6c2caebdf66b95d9415a7c6f7a60835f4aaf5e7d
Meowcelinee <meowcelinee@gmail.com>
2025-08-30 01:35:52 -0400

    add function to display time remaining in class

---
+++ src/display/time/classTimeRemaining.ts
@@ -0,0 +1,25 @@
+import { DateTime, Interval } from 'luxon';
+import { Schedule } from '../../schedules/schoolClassSchedules';
+
+// prettier-ignore
+export default function classTimeRemaining(): string {
+    const currentClass: Schedule = Schedule.fromObject({ name: 'TemporaryClassUntilIImplementMoreShit', start: '12:00', end: '13:45' });
+
+    if (!currentClass) return '';
+
+    return `Time left in this class: ${
+        Interval.fromDateTimes(
+            DateTime.now().toLocal(), // current time (localized)
+            DateTime.fromObject({
+                hour: currentClass.endTime.hour,
+                minute: currentClass.endTime.minute
+            }) // end time
+        )
+        .toDuration(['hours', 'minutes']) // get time between now and end of class
+        .toHuman({
+            unitDisplay: 'short',
+            maximumFractionDigits: 0,
+            showZeros: false
+        }) // make it actually readable
+    }`;
+}
