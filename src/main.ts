7a9c22251c55b235b00ab9e7459e35a2bc3f9cd6
Meowcelinee <meowcelinee@gmail.com>
2025-08-30 01:36:13 -0400

    update page content dynamically

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,22 @@ import './style.css';
 import { classes } from './schedules/schoolClasses';
 import { ClassElement } from './display/classElements';
 import { DateTime } from 'luxon';
+import classTimeRemaining from './display/time/classTimeRemaining';
+import displayTime from './display/time/displayTime';
 
-console.log(
-    'hi this site is currently completely static. the classes are not dynamically generated yet'
-);
-
-document.getElementById('scheduleType')!.textContent =
-    DateTime.local().weekdayLong;
+const scheduleType = document.getElementById('scheduleType')!;
+const timeLeft = document.getElementById('timeLeft')!;
 
 classes?.forEach(cls => {
     document.getElementById('scheduleContent')!.appendChild(ClassElement(cls));
 });
+
+scheduleType.textContent = DateTime.local().weekdayLong;
+timeLeft.textContent = classTimeRemaining();
+displayTime();
+
+setInterval(() => {
+    scheduleType.textContent = DateTime.local().weekdayLong;
+    timeLeft.textContent = classTimeRemaining();
+    displayTime();
+}, 250);
