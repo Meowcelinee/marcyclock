f4a70bbc5cd80fd1beb519d4bfdc10d54b9f086a
Meowcelinee <meowcelinee@gmail.com>
2025-08-30 01:34:25 -0400

    change port number to skys suggestion

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import type { UserConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        port: 3160, // im so straight
+    },
+
     plugins: [tailwindcss()],
 }) satisfies UserConfig;
