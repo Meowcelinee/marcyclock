9cc3f2b930c489e5a83911759da3793f46a2b129
Meowcelinee <meowcelinee@gmail.com>
2025-08-31 00:27:05 -0400

    export classes

--- src/schedules/schoolClasses.ts
+++ src/schedules/schoolClasses.ts
@@ -1,31 +1,31 @@
 import { DateTime } from 'luxon';
 import { Schedule } from './schoolClassSchedules';
 
-const INTREL = Schedule.fromObject({
+export const INTREL = Schedule.fromObject({
     name: 'International Relations',
     start: '20:00',
     end: '21:15',
 });
 
-const CCSUCCESS = Schedule.fromObject({
+export const CCSUCCESS = Schedule.fromObject({
     name: 'College and Career Success',
     start: '21:15',
     end: '22:45',
 });
 
-const PRECALC = Schedule.fromObject({
+export const PRECALC = Schedule.fromObject({
     name: 'Precalculus',
     start: '12:00',
     end: '13:45',
 });
 
-const COMP = Schedule.fromObject({
+export const COMP = Schedule.fromObject({
     name: 'Composition',
     start: '14:15',
     end: '15:45',
 });
 
-const JAVA = Schedule.fromObject({
+export const JAVA = Schedule.fromObject({
     name: 'Java I',
     start: '18:00',
     end: '19:45',
