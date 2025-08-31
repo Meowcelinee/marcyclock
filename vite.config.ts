c366e1633cc89f94c02fa775427553d1da287664
Meowcelinee <meowcelinee@gmail.com>
2025-08-31 00:38:55 -0400

    yeah idk

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import type { UserConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-    server: {
-        port: 3160, // im so straight
-    },
-
+const viteConfig: UserConfig = defineConfig({
+    server: { port: 3160 }, // im so straight
     plugins: [tailwindcss()],
-}) satisfies UserConfig;
+});
+
+export default viteConfig;
